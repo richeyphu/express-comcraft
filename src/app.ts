dbfc1ac740248b615954a1d6910ffa6a2a946228
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-01-27 05:35:19 +0700

    Format with Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
-import express, { Express } from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import express, { Express } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
 const app: Express = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(
   express.json({
-    limit: "50mb",
+    limit: '50mb',
   })
 );
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
