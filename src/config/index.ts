f2916645e2c98596a1df9ce9df66021ae790d57e
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-01-27 05:26:43 +0700

    Config `dotenv`

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT,
+  MONGODB_URI: process.env.MONGODB_URI,
+  DOMAIN: process.env.DOMAIN,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
