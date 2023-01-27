3fe5a03d709d14e1208c5b4d77248806f646f9a6
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-01-27 16:10:16 +0700

    Fix `public` static path

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,10 @@ import usersRouter from './routes/users';
 const app: Express = express();
 
 app.use(logger('dev'));
-app.use(
-  express.json({
-    limit: '50mb',
-  })
-);
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
