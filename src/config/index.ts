d2fab623fd4d48dd21e7552fa9f07409a910a662
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-01-30 14:48:53 +0700

    Fix eslint error

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
+/* eslint-disable node/no-process-env */
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI,
-  DOMAIN: process.env.DOMAIN,
-  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: process.env.PORT ?? 3000,
+  MONGODB_URI: process.env.MONGODB_URI ?? null,
+  DOMAIN: process.env.DOMAIN ?? 'http://localhost:3000',
+  JWT_SECRET: process.env.JWT_SECRET ?? '',
 };
