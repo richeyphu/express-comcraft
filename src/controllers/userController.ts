6dcf1e28edd6d746bf14cf02eb291b6d8d76aa1a
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-02 03:49:40 +0700

    Add `user.register` route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 import config from '@config';
 import { User, IUser } from '@models';
-import { errorHandler } from '@middleware';
+import { IError } from '@middleware';
 
 const index = (req: Request, res: Response, next: NextFunction): void => {
   res.status(200).json({
@@ -12,4 +12,43 @@ const index = (req: Request, res: Response, next: NextFunction): void => {
   });
 };
 
-export { index };
+const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { name, email, password } = req.body as IUser;
+
+    // validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error: IError = new Error('ข้อมูลที่ได้รับมาไม่ถูกต้อง');
+      error.statusCode = 422;
+      error.validation = errors.array();
+      throw error;
+    }
+
+    const existEmail = await User.findOne({ email: email });
+    if (existEmail) {
+      const error: IError = new Error('อีเมลนี้มีผู้ใช้งานในระบบแล้ว');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const user: IUser = new User();
+    user.name = name;
+    user.email = email;
+    user.password = user.encryptPassword(password);
+
+    await user.save();
+
+    res.status(201).json({
+      message: 'เพิ่มข้อมูลเรียบร้อยแล้ว',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { index, register };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import { body } from 'express-validator';
 
 import { userController } from '@controllers';
 
@@ -6,5 +7,25 @@ const router: Router = express.Router();
 
 /* GET users listing. */
 router.get('/', userController.index);
+
+router.post(
+  '/register',
+  [
+    body('name').not().isEmpty().withMessage('กรุณาป้อนชื่อ-สกุลด้วย'),
+    body('email')
+      .not()
+      .isEmpty()
+      .withMessage('กรุณาป้อนอีเมลด้วย')
+      .isEmail()
+      .withMessage('รูปแบบอีเมลไม่ถูกต้อง'),
+    body('password')
+      .not()
+      .isEmpty()
+      .withMessage('กรุณาป้อนรหัสผ่านด้วย')
+      .isLength({ min: 5 })
+      .withMessage('รหัสผ่านต้องมีอย่างน้อย 5 ตัวอักษร'),
+  ],
+  userController.register
+);
 
 export default router;
