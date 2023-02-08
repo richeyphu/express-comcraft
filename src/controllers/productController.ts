372280439b326afe69559a61111ed1f820235827
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-08 12:15:02 +0700

    Add insert and show product function

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,6 +4,7 @@ import { validationResult } from 'express-validator';
 import config from '@config';
 import { Product, IProduct } from '@models';
 import { IError } from '@middleware';
+import { isCorrectOid } from '@utils';
 
 const index = async (
   req: Request,
@@ -22,4 +23,94 @@ const index = async (
   }
 };
 
-export { index };
+const insert = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { name, price, description, brand, category, photo } =
+      req.body as IProduct;
+
+    // validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error: IError = new Error('ข้อมูลที่ได้รับมาไม่ถูกต้อง');
+      error.statusCode = 422;
+      error.validation = errors.array();
+      throw error;
+    }
+
+    const product = new Product({
+      name,
+      price,
+      description,
+      brand,
+      category,
+      photo,
+    });
+    await product.save();
+
+    res.status(201).json({
+      message: 'เพิ่มข้อมูลเรียบร้อยแล้ว',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const showById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const oid = req.params.id;
+
+    if (!isCorrectOid(oid)) {
+      const error: IError = new Error('ไม่พบข้อมูล');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const product: IProduct | null = await Product.findById(oid);
+
+    if (!product) {
+      const error: IError = new Error('ไม่พบข้อมูล');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const showByCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const product: IProduct[] = await Product.find({
+      category: { $regex: new RegExp(req.params.cat, 'i') },
+    });
+
+    if (product.length === 0) {
+      const error: IError = new Error('ไม่พบข้อมูล');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      data: product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { index, insert, showById, showByCategory };
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import { body } from 'express-validator';
 
 import { productController } from '@controllers';
 
@@ -6,5 +7,21 @@ const productRouter: Router = express.Router();
 
 /* GET products listing. */
 productRouter.get('/', productController.index);
+productRouter.get('/q/:id', productController.showById);
+productRouter.get('/:cat', productController.showByCategory);
+
+productRouter.post(
+  '/',
+  [
+    body('name').not().isEmpty().withMessage('กรุณาป้อนชื่อสินค้า'),
+    body('price')
+      .not()
+      .isEmpty()
+      .withMessage('กรุณาป้อนราคา')
+      .isNumeric()
+      .withMessage('ราคาต้องเป็นตัวเลขเท่านั้น'),
+  ],
+  productController.insert
+);
 
 export default productRouter;
