2802cfd4c8e3fa30c34001fe629389249be48a6c
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-12 01:48:32 +0700

    Fix error when not login

--- src/middleware/checkAdmin.ts
+++ src/middleware/checkAdmin.ts
@@ -1,13 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+
 import { IUser } from '@models';
+import { StatusCode } from '@config';
 
 const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    return res.status(StatusCode.UNAUTHORIZED).json({
+      error: {
+        message: 'คุณไม่ได้เข้าสู่ระบบ กรุณาเข้าสู่ระบบก่อนใช้งาน',
+      },
+    });
+  }
+
   const { role } = req.user as IUser;
 
   if (role === 'admin') {
     next();
   } else {
-    return res.status(403).json({
+    return res.status(StatusCode.FORBIDDEN).json({
       error: {
         message: 'คุณไม่มีสิทธิ์ใช้งานส่วนนี้ เฉพาะผู้ดูแลระบบเท่านั้น',
       },
