26e684897982b9debc11c7db9957db1efebb7827
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-15 21:25:43 +0700

    Add `passport.initialized()`

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import passport from 'passport';
 
 import indexRouter from '@routes/index';
 import usersRouter from '@routes/user';
@@ -24,6 +25,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(passport.initialize());
 app.use('/status', serverStatus(app));
 
 app.use('/', indexRouter);
