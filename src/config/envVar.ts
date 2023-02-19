237dbd0bad7eec3f9a01c08d175087db424a8853
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-19 18:32:24 +0700

    Add `MONGODB_URI_TEST`

--- src/config/envVar.ts
+++ src/config/envVar.ts
@@ -6,8 +6,13 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT ?? 3000,
-  MONGODB_URI: process.env.MONGODB_URI ?? null,
+  // MONGODB_URI: process.env.MONGODB_URI ?? null,
   DOMAIN: process.env.DOMAIN ?? 'http://localhost:3000',
   JWT_SECRET: process.env.JWT_SECRET ?? '',
   NODE_ENV: process.env.NODE_ENV ?? 'development',
+  get MONGODB_URI(): string | null {
+    return this.NODE_ENV === 'test'
+      ? process.env.MONGODB_URI_TEST ?? null
+      : process.env.MONGODB_URI ?? null;
+  },
 } as const;
