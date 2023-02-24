dc1b5075928dc6b73186ca364ea550cdd57ba87f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-25 01:27:31 +0700

    Update message :trollface:

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -3,7 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 import { StatusCode } from '@config';
 
 const index = (req: Request, res: Response, next: NextFunction): void => {
-  res.status(StatusCode.I_AM_A_TEAPOT).json({ message: "I'm a teapot!" });
+  res.status(StatusCode.I_AM_A_TEAPOT).json({
+    message:
+      // eslint-disable-next-line max-len
+      '♫ Never gonna give you up ♪ Never gonna let you down ♫ Never gonna run around and desert you ♪ Never gonna make you cry ♫ Never gonna say goodbye ♪ Never gonna tell a lie and hurt you ♫',
+  });
 };
 
 export { index };
