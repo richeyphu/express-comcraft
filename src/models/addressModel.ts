a511f9c08f38f12acc15a36271444133f927bbb5
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-02-24 12:20:04 +0700

    Add address model

---
+++ src/models/addressModel.ts
@@ -0,0 +1,68 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IAddress extends Document {
+  name: string;
+  tel: string;
+  address: {
+    line1: string;
+    line2: string;
+    city: string;
+    province: string;
+    zip: string;
+  };
+  user: Schema.Types.ObjectId;
+}
+
+const schema = new Schema<IAddress>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tel: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      line1: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      line2: {
+        type: String,
+        trim: true,
+      },
+      city: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      province: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      zip: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    collection: 'addresses',
+    timestamps: true,
+  }
+);
+
+const Address = model<IAddress>('Address', schema);
+
+export default Address;
--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,6 @@ export * from './userModel';
 
 export { default as Product } from './productModel';
 export * from './productModel';
+
+export { default as Address } from './addressModel';
+export * from './addressModel';
